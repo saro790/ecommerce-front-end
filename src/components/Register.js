e2f5dc0f43a9b2bb18f5aebb7b10555e5367454d
Sarojini <sarojinidevan2804@gmail.com>
2025-10-16 11:32:58 +0530

    Add back.jpg and update Register page background

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
 import { useAuth } from "../context/AuthContext";
+import backImage from "../assets/images/back.jpg";
 import "../styles/register.css"; // CSS import
 
 function Register() {
